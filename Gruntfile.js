b630e0e636bc32035257d1f20390def2849afadd
zhaoming-mike <zhaoming23@gmail.com>
2015-10-03 19:47:16 +0800

    config change

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,21 @@
-module.exports = function (grunt) {
-  // 项目配置
+锘縨odule.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.file %>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-		//beautify:true,
-		//mangle:false,
-		//compress:true
-      },
-      /*build: {
-        src: 'src/<%=pkg.file %>.js',
-        dest: 'dest/<%= pkg.file %>.min.js'
-      }*/
-	  app_task:{
-		  files:{
-			  'dest/<%= pkg.file %>.min.js':['src/mike-grunt-demo-js-1.js','src/mike-grunt-demo-js-2.js']
-		  }
-	  }
+    sass: {
+      dist: {
+        files: {
+          'style/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
     }
   });
-  // 加载提供"uglify"任务的插件
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  // 默认任务
-  grunt.registerTask('default', ['uglify']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
 }
